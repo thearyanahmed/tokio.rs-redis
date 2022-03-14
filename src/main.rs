12711108b89d36981a087b4efe11aeb7e921e3c5
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-03-14 22:52:01 +0600

    Wip shareded DB

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,77 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use mini_redis::Command::{Get,Set,self};
+use std::collections::HashMap;
+use bytes::Bytes;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String,Bytes>>>;
+type ShardDb = Arc<Vec<Mutex<HashMap<String,Vec<u8>>>>>;
 
 #[tokio::main]
 async fn main() {
-
     let listener = TcpListener::bind("localhost:6379").await.unwrap();
-    
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+
+        let db = db.clone();
+
+        let handle = tokio::spawn(async move{
+            process(socket,db).await;
+        });
+
+        let out = handle.await.unwrap();
+        println!("got {:?}",out)
     }
 }
 
-async fn process(socket: TcpStream) {
-    let mut con = Connection::new(socket);
+fn insert(db: ShardDb,key: &str, value: &str) {
+    let shard = db[hash(key) % db.len()].lock().unwrap();
 
-    if let Some(frame) = con.read_frame().await.unwrap() {
-        println!("GOT : {:?}",frame);
+    shard.insert(key,value);
+}
 
-        let res = Frame::Error("unimplemented".to_string());
+fn new_shard_db(n : usize) -> ShardDb {
+    let mut db = Vec::with_capacity(n);
 
-        con.write_frame(&res).await.unwrap();
+    for _ in 0..n {
+        db.push(Mutex::new(HashMap::new()));
+    }
+
+    return Arc::new(db);
+}
+
+async fn process(socket: TcpStream, db: Db) {
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                let mut db = db.lock().unwrap();
+                // The value is stored as `Vec<u8>`
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                let db = db.lock().unwrap();
+
+                if let Some(value) = db.get(cmd.key()) {
+                    // `Frame::Bulk` expects data to be of type `Bytes`. This
+                    // type will be covered later in the tutorial. For now,
+                    // `&Vec<u8>` is converted to `Bytes` using `into()`.
+                    Frame::Bulk(value.clone())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        // Write the response to the client
+        connection.write_frame(&response).await.unwrap();
     }
 }
